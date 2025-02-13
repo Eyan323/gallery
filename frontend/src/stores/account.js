022b64b90ac6d9a8d1cdadc18b291dce0f4fda54
Eyan323 <bhgf123@naver.com>
2025-02-13 19:02:00 +0900

    Home commit

--- frontend/src/stores/account.js
+++ frontend/src/stores/account.js
@@ -1,3 +1,16 @@
 import {defineStore} from 'pinia'
 
-export const useAccountStore = defineStore("account", {});
+export const useAccountStore = defineStore("account", { // ①
+    state: () => ({
+        checked: false, // ②
+        loggedIn: false, // ③
+    }),
+    actions: {
+        setChecked(val) { // ④
+            this.checked = val;
+        },
+        setLoggedIn(val) { // ⑤
+            this.loggedIn = val;
+        },
+    },
+});
