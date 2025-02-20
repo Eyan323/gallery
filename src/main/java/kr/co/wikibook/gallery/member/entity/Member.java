8ee01532a90713186ed014607f08a1de510a1599
Eyan323 <bhgf123@naver.com>
2025-02-20 21:12:35 +0900

    member commit

--- src/main/java/kr/co/wikibook/gallery/member/entity/Member.java
+++ src/main/java/kr/co/wikibook/gallery/member/entity/Member.java
@@ -5,6 +5,15 @@ import lombok.Getter;
 import org.hibernate.annotations.CreationTimestamp;
 import java.time.LocalDateTime;
 
+//create table members (
+//    id       int auto_increment comment '아이디' primary key,
+//    name     varchar(50)                          not null comment '회원명',
+//    login_id varchar(50)                          not null comment '로그인 아이디',
+//    login_pw varchar(100)                         not null comment '로그인 패스워드',
+//    created  datetime default current_timestamp() not null comment '생성 일시',
+//    constraint members_uk unique (login_id)
+//) comment '회원';
+
 @Getter
 @Entity
 @Table(name = "members")
