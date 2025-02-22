46f888a1cd4ad8bf0707bbe41f127768e5111cf6
Eyan323 <bhgf123@naver.com>
2025-02-22 16:36:11 +0900

    cart commit

--- src/main/java/kr/co/wikibook/gallery/cart/entity/Cart.java
+++ src/main/java/kr/co/wikibook/gallery/cart/entity/Cart.java
@@ -7,6 +7,13 @@ import java.time.LocalDateTime;
 
 import kr.co.wikibook.gallery.cart.dto.CartRead; // ①
 
+//create table carts (
+//    id        int auto_increment comment '아이디' primary key,
+//    member_id int                                  not null comment '회원 아이디',
+//    item_id   int                                  not null comment '상품 아이디',
+//    created   datetime default current_timestamp() not null comment '생성 일시'
+//) comment '장바구니';
+
 @Getter
 @Entity
 @Table(name = "carts")
