a1ec3c177f5eea8ee296e71f63d01ad5819846fc
Eyan323 <bhgf123@naver.com>
2025-02-27 20:44:04 +0900

    controller commit

--- src/main/java/kr/co/wikibook/gallery/common/controller/MainController.java
+++ src/main/java/kr/co/wikibook/gallery/common/controller/MainController.java
@@ -3,6 +3,8 @@ package kr.co.wikibook.gallery.common.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+// 컨트롤러는 하나 이상의 모델과 하나 이상의 뷰를 잇는 다리 역할을 하며 이벤트 등 메인 로직을 담당한다.
+
 @Controller
 public class MainController {
 
