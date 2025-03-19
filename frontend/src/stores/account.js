105d0959ddd73f768d88cd623a71d72e9986c93a
Eyan323 <bhgf123@naver.com>
2025-03-19 20:55:18 +0900

    스토어, 피니아

--- frontend/src/stores/account.js
+++ frontend/src/stores/account.js
@@ -1,3 +1,7 @@
+// 스토어는 여러 컴포넌트가 공유하는 반응형 상태를 중앙에서 관리하도록 하는 기술, 반응형 상태를 일관되고 효율적으로 관리하기 위함이다.
+// 대표적인 기술로 Pinia,Vuex가 있으며 최신 기술은 Pinia로 가볍고 간결한 문법을 제공한다
+// vue 개발팀은 새로운 앱은 Vuex보다 Pinia를 사용할것을 권장하고 있다.
+
 import {defineStore} from 'pinia'
 
 export const useAccountStore = defineStore("account", {
