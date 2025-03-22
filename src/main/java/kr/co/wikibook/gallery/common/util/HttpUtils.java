c7bf3a2b3d485906c1f27f1cb59d6c733fc576a3
Eyan323 <bhgf123@naver.com>
2025-03-22 23:09:44 +0900

    http유틸

--- src/main/java/kr/co/wikibook/gallery/common/util/HttpUtils.java
+++ src/main/java/kr/co/wikibook/gallery/common/util/HttpUtils.java
@@ -9,16 +9,22 @@ public class HttpUtils {
     // 세션 입력
     public static void setSession(HttpServletRequest req, String key, Object value) {
         req.getSession().setAttribute(key, value);
+        // 세션 생성 메서드, 매개변수로 HTTP 요청객체, 세션키, 세션의 값을 받는다.
+        // 로그인에 성공한 사용자의 정보를 저장할때 사용한다.
     }
 
     // 세션 값 조회
     public static Object getSessionValue(HttpServletRequest req, String key) {
         return req.getSession().getAttribute(key);
+        // 세션의 특정 값을 조회하는 메서드
+        // 로그인 사용자 회원 아이디를 조회할 목적으로 사용
     }
 
     // 세션 삭제
     public static void removeSession(HttpServletRequest req, String key) {
         req.getSession().removeAttribute(key);
+        // 세션의 값을 삭제하는 메서드
+        // 로그아웃을 요청한 사용자 세션을 삭제하는 목적
     }
 
     // 쿠키 입력
--- src/main/java/kr/co/wikibook/gallery/item/entity/Item.java
+++ src/main/java/kr/co/wikibook/gallery/item/entity/Item.java
@@ -17,6 +17,7 @@ import kr.co.wikibook.gallery.item.dto.ItemRead; // ①
 //) comment '상품';
 // 상품 테이블
 
+// Entity는 JPA에서 테이블과 매핑되는 객체를 말한다.
 
 @Getter
 @Entity
@@ -26,18 +27,28 @@ public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    // 아이디필드. @ID 애너테이션을 통해 해당 필드가 기본키이며, 테이블의 기본기 칼럼과 매핑된다.
+    // GenerationType.IDENTITY 전략은 DB에 기본키 생성을 일임하는것이다.
 
     @Column(length = 50, nullable = false)
     private String name;
+    // 상품의 이름 필드. @Column 애너테이션을 지정하여 이 필드가 테이블 칼럼과 매핑된다.
+    // 설정 값의 최대 길이는 50, null을 허용하지 않는다.
 
     @Column(length = 100, nullable = false)
     private String imgPath;
+    // 상품의 사진 경로 필드.
+    // 설정 값의 최대 길이는 100, null을 허용하지 않는다.
 
     @Column(nullable = false)
     private Integer price;
+    // 상품의 가격 필드.
+    // null을 허용하지 않는다.
 
     @Column(nullable = false)
     private Integer discountPer;
+    // 상품의 할인율 필드.
+    // null을 허용하지 않는다.
 
     @Column(nullable = false, updatable = false)
     @CreationTimestamp
